a69aa9abb80ea1c2d56c0fc138c7dbeaed410b2d
Giocatory <nivolyas@gmail.com>
2021-07-07 10:28:43 +0300

    07.07.21

---
+++ LessonFive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LessonFive
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] array = { 10, 98, 22, 15, 24, 78, 97, 22, 20, 1, 4, 6, 8, 7, 3, 11 };
+
+            // my array sorted
+            foreach (var i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            SortArray sra = new SortArray();
+            sra.SetArray(array);
+            sra.MySort(0, array.Length - 1);
+
+            foreach (var i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LessonFive/SortArray.cs
@@ -0,0 +1,37 @@
+﻿namespace LessonFive
+{
+    internal class SortArray
+    {
+        private int[] array;
+
+        public int[] SetArray(int[] newArray)
+        {
+            array = newArray;
+            return array;
+        }
+
+        public void MySort(int l, int r)
+        {
+            int i = l;
+            int j = r;
+            int x = array[(l + r) / 2];
+            do
+            {
+                while (array[i] < x) i++;
+                while (array[j] > x) j--;
+                if (i <= j)
+                {
+                    int y = array[i];
+                    array[i] = array[j];
+                    array[j] = y;
+                    i++;
+                    j--;
+                }
+            } while (i < j);
+            if (l < j)
+                MySort(l, j);
+            if (l < r)
+                MySort(i, r);
+        }
+    }
+}
