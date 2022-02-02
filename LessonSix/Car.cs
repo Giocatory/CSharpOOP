480beb3786165cf83c4398825737252d16c3d795
Giocatory <nivolyas@gmail.com>
2022-02-02 11:12:23 +0300

    Chaining Constructors

--- LessonSix/Car.cs
+++ LessonSix/Car.cs
@@ -4,6 +4,8 @@
     {
         private int maxSpeed;
         private int currSpeed;
+
+
         // Chaining Constructors
         public Car() : this(0, 0) { }
         public Car(int maxSpeed) : this(maxSpeed, 0) { }
@@ -13,15 +15,15 @@
             if (maxSpeed >= 0) MaxSpeed = maxSpeed;
             if (currentSpeed >= 0) CurrSpeed = currentSpeed;
         }
-
-        public int MaxSpeed
+        // But you can do it like this (Using only 1 constructor)
+        /*public Car(int maxSpeed = 0, int currentSpeed = 0)
         {
-            get => maxSpeed; private set => maxSpeed = value;
-        }
+            if (maxSpeed >= 0) MaxSpeed = maxSpeed;
+            if (currentSpeed >= 0) CurrSpeed = currentSpeed;
+        }*/
 
-        public int CurrSpeed
-        {
-            get => currSpeed; set => currSpeed = value;
-        }
+
+        public int MaxSpeed { get => maxSpeed; private set => maxSpeed = value; }
+        public int CurrSpeed { get => currSpeed; set => currSpeed = value; }
     }
 }
