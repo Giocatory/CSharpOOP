e09ce6785d8b7e698b659241fa11d76a0c34af4e
Giocatory <nivolyas@gmail.com>
2022-02-02 11:33:33 +0300

    create static class

---
+++ LessonSeven/Program.cs
@@ -0,0 +1,4 @@
+﻿using LessonSeven;
+
+TimeUtilClass.PrintTime(); // 11:32
+TimeUtilClass.PrintDate(); // 02.02.2022
---
+++ LessonSeven/TimeUtilClass.cs
@@ -0,0 +1,16 @@
+﻿namespace LessonSeven
+{
+    internal static class TimeUtilClass
+    {
+        // A static class can only contain static fields and methods
+        public static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+        }
+
+        public static void PrintDate()
+        {
+            Console.WriteLine(DateTime.Today.ToShortDateString());
+        }
+    }
+}
