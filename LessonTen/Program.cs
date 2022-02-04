4eca1fbb1188993f5f4446d7008b73e3b450aad0
Giocatory <nivolyas@gmail.com>
2022-02-04 12:20:43 +0300

    work with exceptions

---
+++ LessonTen/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    /*string s = "qwdqd";
+    int n = Convert.ToInt32(s);
+    Console.WriteLine(n);*/
+
+    int num = 1;
+    if (num == 1)
+    {
+        // throw new exception
+        Exception ex = new("num not be 1");
+        ex.HelpLink = "https://google.com";
+        throw ex;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message); // num not be 1
+    Console.WriteLine(ex.HelpLink); // https://google.com
+
+    /*Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.Data);
+    Console.WriteLine(ex.HelpLink);
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine(ex.ToString());
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.TargetSite);*/
+}
