4cecf84f47f9e873f15c15b01bdd93b6af5e8dd5
Giocatory <nivolyas@gmail.com>
2022-02-04 11:19:21 +0300

    all classes inherit System.Object

---
+++ LessonNine/Program.cs
@@ -0,0 +1,15 @@
+﻿using LessonNine;
+
+SimpleClass sc = new();
+SimpleClass sc2 = new("Mike", 33);
+
+Console.WriteLine(sc); // User: no-name; Age: 0; Class: SimpleClass
+
+// we override base method "ToString()"
+Console.WriteLine(sc2); // User: Mike; Age: 33; Class: SimpleClass
+Console.WriteLine(sc2.ToString()); // User: Mike; Age: 33; Class: SimpleClass
+
+// Checking a reference to the same object
+SimpleClass sc3 = sc2;
+Console.WriteLine(sc2.Equals(sc3)); // True
+Console.WriteLine(sc.Equals(sc3)); // False
---
+++ LessonNine/SimpleClass.cs
@@ -0,0 +1,32 @@
+﻿namespace LessonNine
+{
+    internal class SimpleClass
+    {
+        public SimpleClass() : this("no-name", default) { }
+        public SimpleClass(string name, int age)
+        {
+            Name = name;
+            if (age > 0 || age < 120) Age = age;
+            else Age = default;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // All classes inherit from System.Object by default
+        public override string ToString() => $"User: {Name}; Age: {Age}; Class: {nameof(SimpleClass)}";
+        public override bool Equals(object? obj)
+        {
+            if (obj is SimpleClass && obj != null)
+            {
+                SimpleClass temp = (SimpleClass)obj;
+                if (temp.Name == this.Name && temp.Age == this.Age) return true;
+                else return false;
+            }
+            return false;
+            /*
+             OR we can write simple:
+            public override bool Equals(object? obj) => obj?.ToString() == ToString();
+             */
+        }
+    }
+}
